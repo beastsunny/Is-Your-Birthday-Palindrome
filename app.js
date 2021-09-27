125061732fe1525b5dfdda5f30a3fe6d3d852b5a
Abhishek Kumar <beastsunny06@gmail.com>
2021-09-27 13:37:22 +0530

    feat updated
    reversestr

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const dateEl = document.querySelector("#dob");
 const checkBtn = document.querySelector("#check-btn");
 const output = document.querySelector("#output");
+
+const reverseStr = (str) => str.split("").reverse().join("");
+const isPalindrome = (str) => str === reverseStr(str);
+const convertDateToString = (date) => {
+    let convertedDate = {};
+    if (date.day < 10) {
+        convertedDate.day = "0" + date.day;
+    } else {
+        convertedDate.day = date.day.toString();
+    }
+    if (date.month < 10) {
+        convertedDate.month = "0" + date.month;
+    } else {
+        convertedDate.month = date.month.toString();
+    }
+    convertedDate.year = date.year.toString();
+    return convertedDate;
+};
